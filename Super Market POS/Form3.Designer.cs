fabd2e9378b9cadd40590510a771c4e0a3d1f9ec
Dimethma Ranaweera <ranaweeradimethma@gmail.com>
2024-12-08 19:58:10 +0530

    search bar chreated

--- Super Market POS/Form3.Designer.cs
+++ Super Market POS/Form3.Designer.cs
@@ -29,23 +29,45 @@
         private void InitializeComponent()
         {
             this.lblSupp = new System.Windows.Forms.Label();
+            this.lblsearch = new System.Windows.Forms.Label();
+            this.txtsearch = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblSupp
             // 
             this.lblSupp.AutoSize = true;
             this.lblSupp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSupp.Location = new System.Drawing.Point(233, 161);
+            this.lblSupp.Location = new System.Drawing.Point(12, 9);
             this.lblSupp.Name = "lblSupp";
             this.lblSupp.Size = new System.Drawing.Size(121, 31);
             this.lblSupp.TabIndex = 0;
             this.lblSupp.Text = "Supplier";
+            // 
+            // lblsearch
+            // 
+            this.lblsearch.AutoSize = true;
+            this.lblsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsearch.Location = new System.Drawing.Point(520, 19);
+            this.lblsearch.Name = "lblsearch";
+            this.lblsearch.Size = new System.Drawing.Size(66, 20);
+            this.lblsearch.TabIndex = 1;
+            this.lblsearch.Text = "Search";
+            this.lblsearch.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(592, 19);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(184, 20);
+            this.txtsearch.TabIndex = 2;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.lblsearch);
             this.Controls.Add(this.lblSupp);
             this.Name = "Form3";
             this.Text = "Form3";
@@ -57,5 +79,7 @@
         #endregion
 
         private System.Windows.Forms.Label lblSupp;
+        private System.Windows.Forms.Label lblsearch;
+        private System.Windows.Forms.TextBox txtsearch;
     }
 }
--- Super Market POS/Form3.cs
+++ Super Market POS/Form3.cs
@@ -16,5 +16,10 @@ namespace Super_Market_POS
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
