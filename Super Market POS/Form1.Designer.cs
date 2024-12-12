eb45e94b5be0c998f5886e44ca74d24b2847589e
devindathilina <devindacthilina@gmail.com>
2024-12-12 12:49:16 +0530

    sign in font change

--- Super Market POS/Form1.Designer.cs
+++ Super Market POS/Form1.Designer.cs
@@ -43,11 +43,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.label1.Font = new System.Drawing.Font("Wide Latin", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(472, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(322, 59);
+            this.label1.Size = new System.Drawing.Size(274, 79);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sign In";
             this.label1.Click += new System.EventHandler(this.label1_Click);
